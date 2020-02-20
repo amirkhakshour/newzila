45a34794ba9ff70e4c60fd128db9446aa725b699
Amir Khakshour <amir.khakshour@gmail.com>
2020-02-20 22:59:43 +0100

    TDD-Red: a subscription will be created after user posts it\'s data

--- newzila/newsletter/tests/functional/test_subscription.py
+++ newzila/newsletter/tests/functional/test_subscription.py
@@ -2,6 +2,7 @@ from django.urls import reverse
 
 from newzila.testcases import WebTestCase
 from newzila.newsletter.tests.factories import NewsletterFactory
+from newzila.newsletter.models import Subscription
 
 
 class TestNewsletterViewSet(WebTestCase):
@@ -15,3 +16,4 @@ class TestNewsletterViewSet(WebTestCase):
         print("self.user", self.user)
         response = self.app.post(newsletter_url, params=post_params)
         self.assertEqual(200, response.status_code)  # for newly created instance
+        Subscription.objects.get(email_field=post_params['email'])  # creates the subscription
