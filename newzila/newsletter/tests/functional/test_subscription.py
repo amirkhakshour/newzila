763bccf2af606625b63d63c883b99a674618d32e
Amir Khakshour <amir.khakshour@gmail.com>
2020-02-21 13:45:00 +0100

    - TDD-Refactor:   - Refactored TestSubscriptionDetailView to setup newsletter_subscribe_url on test setup

--- newzila/newsletter/tests/functional/test_subscription.py
+++ newzila/newsletter/tests/functional/test_subscription.py
@@ -9,20 +9,20 @@ class TestNewsletterViewSet(WebTestCase):
     is_anonymous = True
     csrf_checks = False
 
+    def setUp(self):
+        super().setUp()
+        self.newsletter = NewsletterFactory()
+        kwargs = {'pk': self.newsletter.id}
+        self.newsletter_subscribe_url = reverse('api:newsletter-subscribe', kwargs=kwargs)
+
     def test_anonym_user_can_subscribe(self):
-        newsletter = NewsletterFactory()
-        kwargs = {'pk': newsletter.id}
-        newsletter_url = reverse('api:newsletter-subscribe', kwargs=kwargs)
         post_params = {'email_field': 'dummy@example.com'}
-        response = self.app.post_json(newsletter_url, params=post_params)
+        response = self.app.post_json(self.newsletter_subscribe_url, params=post_params)
         self.assertEqual(200, response.status_code)  # for newly created instance
         Subscription.objects.get(email_field=post_params['email_field'])  # creates the subscription
 
     def test_user_can_subscribe(self):
-        newsletter = NewsletterFactory()
-        kwargs = {'pk': newsletter.id}
-        newsletter_url = reverse('api:newsletter-subscribe', kwargs=kwargs)
         user = self.user_1
-        response = self.app.post_json(newsletter_url, user=user)
+        response = self.app.post_json(self.newsletter_subscribe_url, user=user)
         self.assertEqual(200, response.status_code)
-        Subscription.objects.get(user=user)
+        Subscription.objects.get(user=user)  # TODO check for output
